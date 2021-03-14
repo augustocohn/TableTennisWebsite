fc92aef235b7c82b918ed7b782af0a178296699a
FrankAlbella <FrankAlbella@gmail.com>
2021-03-13 22:21:47 -0500

    Add inital announcement functions

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var http = require('http');
 var express = require('express');
+var mongoose = require("mongoose");
 
+const { Announcement } = require("./schema/announcement");
 
 var app = express();
 app.set('port', 3000);
 
-app.use(express.static('public'));
+app.use(express.static('publi\
+c'));
+
+//under normal circumstances, store this in a git-ignored config file or environment variable
+const DB_URI = "";
+
+//connect to db
+mongoose.connect(process.env.DB_URI || DB_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+	console.log("MongoDB database connected");
+});
+connection.on("error", (error) => console.log("Error: " + error));
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -44,6 +64,40 @@ const getFileUpdatedDate = (path) => {
 	const stats = fs.statSync(path);
 	return stats.mtime;
 }
+
+app.post("/api/addannouncement", function(req, res) {
+	log("addannouncement");
+	try {
+		Announcement.create({ title: req.body.title, body: req.body.body, date_created: Date.now() });
+
+		return res.json({ message: "Successfully created announcement." });
+	} catch (error) {
+		return res.json({ message: error });
+	}
+});
+
+app.put("/api/editannouncement", function(req, res) {
+	log("editannouncement");
+});
+
+app.delete("/api/removeannouncement/:id", function(req, res) {
+	log("removeannouncement");
+	Announcement.findByIdAndDelete(req.query.id);
+});
+
+app.get("/api/getannouncements", function(req, res) {
+	log("getannouncements");
+
+	var limit = parseInt(req.query.count || 5);
+	Announcement.find().sort({ _id: -1 }).limit(limit).exec(function(err, posts){
+		if(err){
+			console.log(err);
+		}
+		else{
+			return res.send(posts);
+		}
+	});
+});
 
 app.use(function (req, res) {
 	res.type('text/plain');
