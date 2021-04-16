07460b7d36d1dd4d85f53a96cdc5670358b07654
FrankAlbella <FrankAlbella@gmail.com>
2021-04-15 21:33:24 -0400

    Update tournament matches schema
    Tournament matches are now nested arrays

--- schema/tournament.js
+++ schema/tournament.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TournamentSchema = new mongoose.Schema({
     players: [{fullname: String, wins: Number}],
-    matches: [{playerone: String, playertwo: String, winner: Number}],
+    matches: [[{playerone: String, playertwo: String, winner: Number}]],
     date: {
         type: Number,
         required: true
